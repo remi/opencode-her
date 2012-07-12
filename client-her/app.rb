add8dbbb914cd8b1a29b3626b693f84add8d81f8
Rémi Prévost <remi@exomel.com>
2012-07-12 14:25:01 -0400

    Remove OAuth credentials

--- client-her/app.rb
+++ client-her/app.rb
@@ -1,16 +1,8 @@
 require "bundler"
 Bundler.require
 
-OAUTH_CREDENTIALS = {
-  :consumer_key => "foo",
-  :consumer_secret => "bar",
-  :token => "lol",
-  :token_secret => "omg"
-}
-
 Her::API.setup :base_uri => "http://localhost:4002", do |connection|
   connection.use Her::Middleware::AcceptJSON
-  connection.use FaradayMiddleware::OAuth, OAUTH_CREDENTIALS
   connection.use Faraday::Request::UrlEncoded
   connection.use Her::Middleware::DefaultParseJSON
   connection.use Faraday::Adapter::NetHttp
