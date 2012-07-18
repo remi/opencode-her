ec14dc7ef4c690949cfbdec80f1feb3ace5af7a5
Rémi Prévost <remi@exomel.com>
2012-07-18 10:12:44 -0400

    Add OAuth and HTTP caching middleware

--- client-her/app.rb
+++ client-her/app.rb
@@ -2,12 +2,23 @@ require "logger"
 require "bundler"
 Bundler.require
 
+OAUTH_CREDENTIALS = {
+  :consumer_key => "foo",
+  :consumer_secret => "bar",
+  :token => "lol",
+  :token_secret => "omg"
+}
+
+$dalli = Dalli::Client.new("localhost:11211", :namespace => "her-demo")
+
 # Her
 # -------------------------------------------------------------
-Her::API.setup :base_uri => "http://localhost:4002", do |c|
+Her::API.setup :url => "http://localhost:4002", do |c|
   # Request middleware
   c.use Her::Middleware::AcceptJSON
   c.use Faraday::Request::UrlEncoded
+  c.use FaradayMiddleware::OAuth, OAUTH_CREDENTIALS
+  c.use FaradayMiddleware::Caching, $dalli
 
   # Response middleware
   c.use Faraday::Response::Logger, Logger.new(STDOUT, :level => Logger::INFO).tap { |l| l.level = Logger::INFO }
